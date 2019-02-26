89e0e8213f8a17d460745251468fd3d1be9641f7
João Victor <jvitorgmodesto@gmail.com>
2019-02-26 15:25:55 -0300

    Método Get

--- index.php
+++ index.php
@@ -5,10 +5,32 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require 'bootstrap.php';
 
 
-$app->get('/', function (Request $request, Response $response) use ($app) {
+$app->get('/cliente/{id}', function (Request $request, Response $response) use ($app) {
    
-	 
+    $id = $request->getAttribute("id");
+    
+    $con = getConnection();
+    
+    $stm = $con->prepare("SELECT * FROM CLIENTES WHERE ID =:id ");
+    $stm->bindParam("id", $id);
+    
+    try{
+   $stm->execute();
+    }catch(PDOException $e){
+        return $e->getMessage();
+    }
+    
+    if($cliente = $stm->fetchAll(PDO::FETCH_ASSOC)){
+    
+    $return = $response->withJson($cliente, 200)->withHeader('Content-type', 'application/json');
+    }
+    else{
+        $return = $response->withJson("Not Found", 404)->withHeader('Content-type', 'application/json');
+    }
+	 return $return;
 });
+
+
 
 $app->post('/novo', function(Request $request, Response $response) use ($app){
 
@@ -35,8 +57,6 @@ $app->post('/novo', function(Request $request, Response $response) use ($app){
 	}catch(Excpetion $e){
 		throw new Exception($ex->getMessage(), $ex->getCode());
 	}
-
-
 
 });
 
